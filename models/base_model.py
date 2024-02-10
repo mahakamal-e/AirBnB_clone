4099129f77ea53ba8c8f3b3aa25b8102a18bedbf
mahakamal-e <mahakamal09@gmail.com>
2024-02-10 07:29:42 -0800

    add

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ import models
 
 class BaseModel:
     """
-    Main class used defines all common attributes/methods for other classes
+    Main class that  defines all common attributes/methods for other classes
     """
     def __init__(self, *args, **kwargs):
         """
